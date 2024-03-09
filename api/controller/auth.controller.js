703dc06cece2c312b38247b0f26faee9bc9474dc
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-03-09 12:03:09 +0530

    complete signin api route backend code

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,7 @@
 import userModel from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import {errorHandle} from "../utils/error.js";
 
 export const signUp = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -8,6 +10,21 @@ export const signUp = async (req, res, next) => {
     try{
         await newUser.save();
         res.status(201).json("user created successfully");
+    }catch(error){
+        next(error);
+    }
+}
+
+export const signIn = async (req, res, next) => {
+    const {email, password} = req.body;
+    try{
+        const vaildUser = await userModel.findOne({email});
+        if(!vaildUser) return next(errorHandle(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, vaildUser.password);
+        if(!validPassword) return next(errorHandle(404, "Wrong Credentials"));
+        const token = jwt.sign({id: vaildUser._id}, process.env.JWT_SECRET);
+        const {password: pass, ...rest} = vaildUser._doc;
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
     }catch(error){
         next(error);
     }
