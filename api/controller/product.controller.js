932ac26ce24a7fbab7d55b519fb2916e0ab3ee12
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-03-15 12:53:24 +0530

    product update api route

--- api/controller/product.controller.js
+++ api/controller/product.controller.js
@@ -2,26 +2,45 @@ import productModel from "../models/product.model.js";
 import { errorHandle } from "../utils/error.js";
 
 export const createProduct = async (req, res, next) => {
-    try{
+    try {
         const product = await productModel.create(req.body);
         return res.status(201).json(product);
-    }catch(error){
+    } catch (error) {
         next(error);
     }
 };
 
 export const deleteProduct = async (req, res, next) => {
     const product = await productModel.findById(req.params.id);
-    if(!product) return next(errorHandle(404, "Product not found"));
+    if (!product) return next(errorHandle(404, "Product not found"));
 
-    if(req.user.id !== product.userRef){
+    if (req.user.id !== product.userRef) {
         return next(errorHandle(401, "You can only delete your own porducts"));
     }
 
-    try{
+    try {
         await productModel.findByIdAndDelete(req.params.id);
         res.status(200).json("product has been deleted");
-    }catch(error){
+    } catch (error) {
         next(error);
     }
 };
+
+export const updateProduct = async (req, res, next) => {
+    const product = await productModel.findById(req.params.id);
+    if (!product) {
+        return next(errorHandle(404, 'Product not found!'));
+    }
+    if (req.user.id !== product.userRef) {
+        return next(errorHandle(401, 'You can only update your own Product!'));
+    }
+
+    try {
+        const updatedProduct = await productModel.findByIdAndUpdate(
+            req.params.id, req.body, { new: true }
+        );
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        next(error);
+    }
+}
--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createProduct, deleteProduct } from "../controller/product.controller.js";
+import { createProduct, deleteProduct, updateProduct } from "../controller/product.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createProduct);
 router.delete("/delete/:id", verifyToken, deleteProduct);
+router.post("/update/:id", verifyToken, updateProduct);
 
 export default router;
