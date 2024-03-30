3c8802525af4fc3eb2e12f790e40169e09cf7004
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-03-30 20:22:31 +0530

    create header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../public/images/logo.png";
 import { IoSearch } from "react-icons/io5";
 import { FaCartShopping } from "react-icons/fa6";
@@ -9,6 +9,26 @@ import { useSelector } from 'react-redux';
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   // console.log("current user is ", currentUser);
+  const [searchTerm, setSearchTerm] = useState('');
+  // console.log("searchTerm is ", searchTerm);
+  const navigate = useNavigate();
+
+  useEffect(()=> {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
 
   return (
     <header className=''>
@@ -23,8 +43,8 @@ export default function Header() {
           <Link to='/about' className='text-lg'>About</Link>
           <Link to='/contact' className='text-lg'>Contact</Link>
         </div>
-        <form action="" className="w-[40%] pr-8 bg-white relative rounded-full overflow-hidden">
-          <input type="text" className="w-full px-4 py-2 outline-none" placeholder='Search...' />
+        <form onSubmit={handleSubmit} className="w-[40%] pr-8 bg-white relative rounded-full overflow-hidden">
+          <input type="text" className="w-full px-4 py-2 outline-none" placeholder='Search...' value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)} />
           <button className='absolute top-2 right-3 text-2xl'><IoSearch />
           </button>
         </form>
