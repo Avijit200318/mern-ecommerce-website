89fa905a3afd690529fd3b8f20800bdfc472fdc8
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-04-07 12:49:49 +0530

    update header responsiveness

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,6 +6,8 @@ import { FaCartShopping } from "react-icons/fa6";
 import { BsBoxSeamFill } from "react-icons/bs";
 import { useSelector } from 'react-redux';
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { IoMenu } from "react-icons/io5";
+import { RxCross2 } from "react-icons/rx";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
@@ -15,11 +17,13 @@ export default function Header() {
   const navigate = useNavigate();
   const [menu, setMenu] = useState(false);
   const [menuOption, setMenuOption] = useState(null);
+  const [menuBtnOption, setMenuBtnOption] = useState(false);
+  console.log(menuBtnOption)
 
-  useEffect(()=> {
+  useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get('searchTerm');
-    if(searchTermFromUrl){
+    if (searchTermFromUrl) {
       setSearchTerm(searchTermFromUrl);
     }
   }, [location.search]);
@@ -41,10 +45,10 @@ export default function Header() {
     setMenuOption(e.target.id);
   }
 
-  useEffect(()=> {
+  useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const type = urlParams.get('type');
-    if(type){
+    if (type) {
       const value = {
         'phone': 'Smart Phone',
         'computer': 'Laptops',
@@ -55,28 +59,34 @@ export default function Header() {
       }
       const option = value[type];
       setMenuOption(option);
-    }else{
+    } else {
       setMenuOption("Explore");
     }
   }, [location.search]);
 
+  const handleMenuBtn = () => {
+    setMenuBtnOption(!menuBtnOption);
+  }
+
   return (
     <header id='header' className=''>
-      <div className="flex w-full justify-between items-center bg-[#0d6efd] px-10 py-2">
-        <div className="flex gap-12 items-center w-[70%]">
-        <div className="nameLogo flex items-center">
-          <img src={Logo} alt="" className="w-8" />
-          <Link to='/'>
-            <h1 className="text-xl"><span className='text-[#EBF400] font-semibold'>Nova</span><span className="text-[#7FC7D9] font-semibold">Mart</span></h1>
-          </Link>
-        </div>
-        <div onClick={handleMenu} className="relative w-24 bg-white cursor-pointer py-1 rounded-md">
-          <div className="flex gap-1 justify-center items-center">
-          <p className="text-blue-600 font-semibold truncate pl-1">{menuOption? menuOption :'Explore'}</p>
-          <p className="text-xl text-blue-600 pr-1"><MdKeyboardArrowDown /></p>
+      <div className="px-2 flex w-full justify-between items-center bg-[#0d6efd] sm:px-10 py-2">
+        <div className="flex gap-1 items-center w-[70%] sm:gap-12">
+          <div className="nameLogo flex items-center">
+            <Link to='/'>
+              <img src={Logo} alt="" className="w-8" />
+            </Link>
+            <Link to='/'>
+              <h1 className="hidden sm:block text-xl"><span className='text-[#EBF400] font-semibold'>Nova</span><span className="text-[#7FC7D9] font-semibold">Mart</span></h1>
+            </Link>
           </div>
-          <div className="absolute z-30 w-full">
-              <ul className={`bg-white ${menu? 'block':'hidden'} w-32 shadow-xl border-2`}>
+          <div onClick={handleMenu} className="hidden lg:block relative w-24 bg-white cursor-pointer py-1 rounded-md">
+            <div className="flex gap-1 justify-center items-center">
+              <p className="text-blue-600 font-semibold truncate pl-1">{menuOption ? menuOption : 'Explore'}</p>
+              <p className="text-xl text-blue-600 pr-1"><MdKeyboardArrowDown /></p>
+            </div>
+            <div className="absolute z-30 w-full">
+              <ul className={`bg-white ${menu ? 'block' : 'hidden'} w-32 shadow-xl border-2`}>
                 <Link to='/search?type=phone'><li onMouseEnter={handleMouseEnter} id='Smart Phones' className='p-2 border-b-2 hover:bg-blue-500 hover:text-white hover:border-white'>Smart Phones</li></Link>
                 <Link to='/search?type=computer'><li onMouseEnter={handleMouseEnter} id='Laptops' className='p-2 border-b-2 hover:bg-blue-500 hover:text-white hover:border-white'>Laptops</li></Link>
                 <Link to='/search?type=headset'><li onMouseEnter={handleMouseEnter} id='Headsets' className='p-2 border-b-2 hover:bg-blue-500 hover:text-white hover:border-white'>Headsets</li></Link>
@@ -84,20 +94,21 @@ export default function Header() {
                 <Link to='/search?type=boots'><li onMouseEnter={handleMouseEnter} id='Sneakers' className='p-2 border-b-2 hover:bg-blue-500 hover:text-white hover:border-white'>Sneakers</li></Link>
                 <Link to='/search?type=camera'><li onMouseEnter={handleMouseEnter} id='Camera' className='p-2 border-b-2 hover:bg-blue-500 hover:text-white hover:border-white'>Camera</li></Link>
               </ul>
+            </div>
           </div>
+          <form onSubmit={handleSubmit} className="w-[80%] pr-8 bg-white relative rounded-full overflow-hidden sm:w-[60%]">
+            <input type="text" className="w-full px-4 py-2 outline-none" placeholder='Search...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+            <button className='absolute top-2 right-3 text-2xl'><IoSearch />
+            </button>
+          </form>
         </div>
-        <form onSubmit={handleSubmit} className="w-[60%] pr-8 bg-white relative rounded-full overflow-hidden">
-          <input type="text" className="w-full px-4 py-2 outline-none" placeholder='Search...' value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)} />
-          <button className='absolute top-2 right-3 text-2xl'><IoSearch />
-          </button>
-        </form>
-        </div>
-
-        <div className="allBtns flex justify-between items-center gap-4 text-white font-semibold">
+        <button onClick={handleMenuBtn} className="text-white text-3xl sm:hidden"><IoMenu /></button>
+        <div style={{right: menuBtnOption ? '0' : '-100%' }} className="allBtns flex flex-row-reverse items-center gap-4 w-full text-white font-semibold absolute  top-2 right-0 bg-[#0d6efd] z-20 transition-all duration-300 px-2 sm:flex sm:z-0 sm:static sm:right-auto sm:top-auto sm:flex-row sm:justify-between sm:w-auto">
+        <button onClick={handleMenuBtn} className="text-2xl text-white font-semibold pr-2 sm:hidden"><RxCross2 /></button>
           <Link title='Order' to="/order" className="text-2xl  relative"><BsBoxSeamFill />
-          {currentUser && currentUser.order.length > 0 && <span className='bg-red-600 w-[15px] h-[15px] rounded-full absolute top-[-1px] right-[-8px] flex items-center justify-center text-[0.7rem]'>{currentUser.order.length}</span>}
+            {currentUser && currentUser.order.length > 0 && <span className='bg-red-600 w-[15px] h-[15px] rounded-full absolute top-[-1px] right-[-8px] flex items-center justify-center text-[0.7rem]'>{currentUser.order.length}</span>}
           </Link>
-          <Link title='Cart' to="/cart" className="text-2xl mx-4 relative"><FaCartShopping />
+          <Link title='Cart' to="/cart" className="text-2xl relative sm:mx-4"><FaCartShopping />
             {currentUser && currentUser.cart.length > 0 && <span className='bg-red-600 w-[15px] h-[15px] rounded-full absolute top-[-4px] right-[-4px] flex items-center justify-center text-[0.7rem]'>{currentUser.cart.length}</span>}
           </Link>
           {currentUser ? (
@@ -105,7 +116,7 @@ export default function Header() {
               <img src={currentUser.avatar} alt="" className='w-[2.5rem] h-[2.5rem] border-2 border-white object-cover rounded-full overflow-hidden bg-[#96EFFF]' />
             </Link>
           ) : (
-            <Link to='/sign-in' className="text-xl mx-4 mb-[5px]">Login</Link>
+            <Link to='/sign-in' className="text-xl mb-[5px] sm:mx-4">Login</Link>
           )}
         </div>
       </div>
